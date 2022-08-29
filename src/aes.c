dea52523af0aceb4df5eee01a2a56440a0d5b813
Cam <cam@CamdeMacBook-Pro.local>
2022-08-29 17:28:33 +0800

    init

---
+++ src/aes.c
@@ -0,0 +1,101 @@
+#include "aes.h"
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+void handleErrors(void)
+{
+    unsigned long errCode;
+
+    printf("An error occurred\n");
+    while(errCode = ERR_get_error())
+    {
+        char *err = ERR_error_string(errCode, NULL);
+        printf("%s\n", err);
+    }
+    abort();
+}
+
+int encrypt(unsigned char *plaintext, int plaintext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx = NULL;
+    int len = 0, ciphertext_len = 0;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+    /* Initialise the encryption operation. */
+    /* Initialise key and IV */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(plaintext)
+    {
+        if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+            handleErrors();
+
+        ciphertext_len = len;
+    }
+
+    /* Finalise the encryption. Normally ciphertext bytes may be written at
+     * this stage, but this does not occur in GCM mode
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+    ciphertext_len += len;
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+    return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx = NULL;
+    int len = 0, plaintext_len = 0, ret;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+    /* Initialise the decryption operation. */
+    if(!EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary
+     */
+    if(ciphertext)
+    {
+        if(!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+            handleErrors();
+
+        plaintext_len = len;
+    }
+
+    /* Finalise the decryption. A positive return value indicates success,
+     * anything else is a failure - the plaintext is not trustworthy.
+     */
+    ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    if(ret > 0)
+    {
+        /* Success */
+        plaintext_len += len;
+        return plaintext_len;
+    }
+    else
+    {
+        /* Verify failed */
+        return -1;
+    }
+}
